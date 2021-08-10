df4c6fb486688f13273c7d508ceb2850994d2f8c
Bat-Ulzii Orgil <orgil9506@gmail.com>
2021-08-09 19:02:45 -0500

    Initial commit

---
+++ src/test/java/com/jorjill/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorjill.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
